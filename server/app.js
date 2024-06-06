9f3b0c496c4315eff514c2a56a7f929809d14f66
TomReece2T <tom.reece@2t-security.com>
2024-06-06 11:38:25 +0100

    gitignore and serve files from public

--- server/app.js
+++ server/app.js
@@ -3,13 +3,15 @@ const path = require('path');
 
 const app = express();
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../build')));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+// Route for the home page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
+// Use the port defined by Heroku or default to 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
