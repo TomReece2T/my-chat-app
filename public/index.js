37eea7cbf2de966fad4ed08a80b394ab3bdabba6
TomReece2T <tom.reece@2t-security.com>
2024-06-10 15:26:02 +0100

    back to cdn

--- public/index.js
+++ public/index.js
@@ -23,58 +23,59 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log("Security Labels plugin is initialized!");
       _converse.log.log("Security Labels plugin is initialized!");
 
-      // _converse.api.listen.on('beforeMessageSend', function addSecurityLabel(message){
-      //   _converse.log.log("addSecurityLabel() start");
-      //   console.log('addSecurityLabel() start')
-      //   const securityLabel = "Confidential"; 
-      //   if (message.body){
-      //     message.body = `[${securityLabel}] ${message.body}`;
-      //     console.log('Added security label to message:', message.body);
-      //   }
-      // });
-
-      // _converse.api.listen.on('getToolbarButtons', (toolbar_el, buttons) => {
-      //   console.log('do something to toolbar buttons, toolbar_el, buttons', toolbar_el, buttons)
-      //   const mybutton = document.createElement('button');
-      //   mybutton.id = 'mybutton';
-      //   mybutton.textContent = 'Mark as SECRET';
-      //   mybutton.onclick = function() {
-      //     console.log('you made a message SECRET')
-      //     alert('you made a message SECRET')
-      //   }
-      //   buttons.push(mybutton);
-      //   return buttons;
-      // });
-
       _converse.on('connected', function () {
+
+        _converse.api.listen.on('getToolbarButtons', (toolbar_el, buttons) => {
+          console.log('do something to toolbar buttons, toolbar_el, buttons', toolbar_el, buttons)
+          const mybutton = document.createElement('button');
+          mybutton.id = 'mybutton';
+          mybutton.textContent = 'Mark as SECRET';
+          mybutton.onclick = function() {
+            console.log('you made a message SECRET')
+            alert('you made a message SECRET')
+          }
+          buttons.push(mybutton);
+          return buttons;
+        });
+
         _converse.api.listen.on('getOutgoingMessageAttributes', (chat, attrs) => {
           console.log('1. on getOutgoingMessageAttributes, chat, attrs', attrs);
+          const newAttrs = {...attrs}
+          newAttrs['body'] = '[SECRET] ' + newAttrs['body']
+          // newAttrs['is_headline'] = false
+
+      //     newAttrs['securitylabel'] = `
+      //     <securitylabel xmlns="urn:xmpp:sec-label:0">
+      //         <displaymarking>CONFIDENTIAL</displaymarking>
+      //         <esssecuritylabel>
+      //             <classification>1</classification>
+      //             <category>Test</category>
+      //         </esssecuritylabel>
+      //     </securitylabel>
+      // `;
+
+          console.log('sending message with newAttrs', newAttrs)
+
+          return newAttrs
         });
+
+        // _converse.api.listen.on('createMessageStanza', (chat, data) => {
+        //   console.log('1. createMessageStanza chat, data:', chat, data);
+
+        //   const securityLabel = data['message']['attributes']['securitylabel']
+
+        //   let newData = {...data}
+        //   newData['stanza']['node']['_testproperty'] = 'abc'
+        
+
+        //   console.log('2. createMessageStanza chat, data:', chat, newData);
+        //   return newData;
+        // });
+
       })
-
-      // _converse.api.listen.on('getOutgoingMessageAttributes', (chat, attrs) => {
-      //   console.log('1. on getOutgoingMessageAttributes, chat, attrs', attrs);
-      // });
-
-      // _converse.api.listen.on('createMessageStanza', (data) => {
-      //   console.log('1. on createMessageStanza, data', data)
-      // });
-
-
 
     },
 
-    // addSecurityLabel(message) {
-    //   console.log('addSecurityLabel() start, message', message)
-    //   const securityLabel = "Confidential";
-
-    //   if (message.body){
-    //     message.body = `[${securityLabel}] ${message.body}`;
-    //     console.log('Added security label to message:', message.body);
-    //   }
-    // },
-
   });
-
 
 });
